3c5db9899a05fd5778b1dccd7a78d71c84a047b0
patelrutvi <patelrutvi107@gmail.com>
2023-06-20 13:18:35 +0530

    drprdata

--- src/containers/Doctor.js
+++ src/containers/Doctor.js
@@ -2,7 +2,23 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 function Doctor(props) {
-    const {id} = useParams()
+    const { id } = useParams()
+
+    let getdata = JSON.parse(localStorage.getItem("drdata"))
+    console.log(getdata, "get");
+
+    
+    let dataidd = []
+
+    getdata.map((v) => {
+        dataidd.push(v.id)
+
+    })
+    console.log(dataidd, 'arr idd');
+   
+    let data = getdata.filter((f) => f.id === dataidd)
+ 
+    console.log(data, "data get filter");
 
     return (
         <div>
