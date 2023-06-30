7fa4e48f99f0c811b47c821e015d5df6b89f1384
patelmonik <patelmonik1110@gmail.com>
2023-06-30 13:33:40 +0530

    ediit dialog open

--- src/admin/components/AMedicine.js
+++ src/admin/components/AMedicine.js
@@ -103,6 +103,21 @@ export default function AMedicine() {
 
     }
 
+    const handleEdit = (ei) => {
+        console.log("edit", ei);
+
+        let mgetlocaldata = JSON.parse(localStorage.getItem("medicine"))
+        console.log(mgetlocaldata);
+
+        let fdata = mgetlocaldata.filter((v, index) => v.id === ei)
+        console.log(fdata);
+
+        handleClickOpen()
+
+        let mn = fdata[0].mname
+        console.log(mn);    
+    }
+
     // var d = new Date();
     // let nd = new Date(d.setDate(d.getDate()-1))
     // ..present date mathi -1
@@ -158,33 +173,34 @@ export default function AMedicine() {
         { field: 'mname', headerName: 'Medicine Name', width: 130 },
         { field: 'expdate', headerName: 'Exp Date', width: 130 },
         { field: 'mprice', headerName: 'Price', width: 130 },
-        { field: 'mdisc', headerName: 'Description', width: 130 },
+        { field: 'mdisc', headerName: 'Description', width: 160 },
         {
             field: 'action',
-            headerName: 'Action',
+            headerName: 'Remove',
             width: 130,
 
             renderCell: (params) => (
                 <DeleteIcon onClick={() => handledelete(params.row.id)}>
 
                 </DeleteIcon>
+
+            ),
+
+        },
+        {
+            field: 'edit',
+            headerName: 'Edit',
+            width: 130,
+            renderCell: (params) => (
+                <EditIcon onClick={() => handleEdit(params.row.id)}>
+
+                </EditIcon>
             )
 
         },
 
     ];
 
-    // const rows = [
-    //     { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    //     { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    //     { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    //     { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    //     { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    //     { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    //     { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    //     { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    //     { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    // ];
 
     return (
         <div>
