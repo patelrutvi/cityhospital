ca9d3d280db3ebf6b3fbc1c0b0a05676acd4f396
patelrutvi <rutvipatel107@gmail.com>
2023-07-06 12:29:17 +0530

    complite 6-july-topic

--- src/Routes/Protected.js
+++ src/Routes/Protected.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 import { Navigate, Outlet, redirect, useNavigate } from 'react-router-dom';
 
 function Protected(props) {
-    const navigate = useNavigate();
     let login = localStorage.getItem("login")
+    console.log(login,"login protected");
     return (
         login ? <Outlet /> : <Navigate to={'/auth'} replace />
            
