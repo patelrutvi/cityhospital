b59b005faf583293e2ab424196e4b2fcacdd0005
patelrutvi <rutvipatel107@gmail.com>
2023-07-19 09:53:41 +0530

    joson data

--- src/redux/action/doctordata.action.js
+++ src/redux/action/doctordata.action.js
@@ -20,3 +20,55 @@ export const addDoctor = (data) => (dispatch) => {
         console.log(error);
     }
 }
+
+
+// {
+//     "posts": [
+//       {
+//         "id": 1,
+//         "title": "json-server",
+//         "author": "typicode"
+//       }
+//     ],
+//     "comments": [
+//       {
+//         "id": 1,
+//         "body": "some comment",
+//         "postId": 1
+//       }
+//     ],
+//     "profile": {
+//       "name": "typicode"
+//     },
+//     "doctors": [
+//       {
+//         "id": 101,
+//         "name": "Dr. Chandnani Sonia",
+//         "Designation": "Gynecologist",
+//         "Degree": "MBBS, MD",
+//         "img":" vbhnvnvb"
+//       },
+//       {
+//         "id": 102,
+//         "name": "Dr. Desai Prashant",
+//         "Designation": "ENT Surgeon",
+//         "Degree": "MBBS",
+//         "img":" ",
+//          "img1":" "
+//       },
+//       {
+//         "id": 103,
+//         "name": "Dr. Rupareliya Ravi",
+//         "Designation": "Physician (MD Medicine)",
+//         "Degree": "MBBS, MD (Medicine)",
+//         "img":" "
+//       },
+//       {
+//         "id": 104,
+//         "name": "Dr. Rashiniha Virendra",
+//         "Designation": "Gynecologist",
+//         "Degree": "BEMS",
+//         "img":" "
+//       }
+//     ]
+//   }
