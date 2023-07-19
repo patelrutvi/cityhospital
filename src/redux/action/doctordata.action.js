3072368e587ec1a5160099835a619840928b3423
patelrutvi <rutvipatel107@gmail.com>
2023-07-19 10:13:33 +0530

    add drdata

--- src/redux/action/doctordata.action.js
+++ src/redux/action/doctordata.action.js
@@ -1,22 +1,32 @@
 import * as ActionType from '../ActionType'
 
 export const getdoctordata = () => (dispatch) => {
-    try{
+    try {
         fetch("http://localhost:3004/doctors")
-        .then((response) => response.json())
-        .then((data) => dispatch({type : ActionType.GET_DOCTORDATA , paylord : data}))
-        .catch((error)=>console.log(error))
-    }catch(error){
+            .then((response) => response.json())
+            .then((data) => dispatch({ type: ActionType.GET_DOCTORDATA, paylord: data }))
+            .catch((error) => console.log(error))
+    } catch (error) {
         console.log(error);
     }
-   
+
 }
 
 
 export const addDoctor = (data) => (dispatch) => {
-    try{
-        fetch("http://localhost:3004/doctors")
-    }catch(error){
+    try {
+        fetch("http://localhost:3004/doctors", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error))
+
+    } catch (error) {
         console.log(error);
     }
 }
