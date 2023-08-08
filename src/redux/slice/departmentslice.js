91255509e5cfa8fd5e89b01c2c9066115a12bef7
patelrutvi <rutvipatel107@gmail.com>
2023-08-08 21:20:42 +0530

    departmentslicehalf

--- src/redux/slice/departmentslice.js
+++ src/redux/slice/departmentslice.js
@@ -1,35 +1,37 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { getDepartmentdata } from "../../common/apis/department.api"
+import { Action } from "@remix-run/router"
+
+const initstate = {
+    isloading : false,
+    depData : [] ,
+    error:null
+}
 
 
+export const fetchDepartment = createAsyncThunk (
+    'department/fetch',
+    async () => {
+        await new Promise ((resolve , reject) => setTimeout(resolve,1000))
+        let response = await getDepartmentdata()
+        return response.data
+    }
+)
 
-// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+export const departmentslice = createSlice ({
+    name:'depData',
+    initialState:initstate,
+    reducers:{},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchDepartment.fulfilled,(state,action) => {
+                console.log(action);
+                state.depData = action.payload
+            })
+    }
+})
 
-// import { resolvePath } from "react-router-dom"
-// import { addDepartmentData, deleteDepartmentData, getDepartmentdata, updateDepartmentData } from "../../common/apis/department.api"
 
-// const initState = {
-//     isloading: false,
-//     depart: [],
-//     error: null,
-// }
-
-// export const fetchDepartments = createAsyncThunk(
-//     'department/fetch',
-
-//     async () => {
-//         await new Promise((resolve, reject) => setTimeout(resolve, 1000))
-//         let response = await getDepartmentdata()
-//         return response.data;
-//     }
-
-// )
-
-// export const addDepartments = createAsyncThunk(
-//     'department/add',
-//     async (data) => {
-//         let response = await addDepartmentData(data)
-//         return response.data
-//     }
-// )
 
 // export const deleteDepartments = createAsyncThunk(
 //     'department/delete',
