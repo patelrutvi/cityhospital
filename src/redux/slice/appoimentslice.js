762adc004b2a3513de43f2b087d4ede6141ee14f
patelrutvi <rutvipatel107@gmail.com>
2023-08-17 09:30:23 +0530

    firebase crud

--- src/redux/slice/appoimentslice.js
+++ src/redux/slice/appoimentslice.js
@@ -1,15 +1,37 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs, doc, deleteDoc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase";
-import {  getDocs } from "firebase/firestore";
+
 
 const initState = {
-    isLoading: false,
+    isloading: false,
     apt: [],
     error: null
 }
+export const getappoinment = createAsyncThunk(
+    'appointment/get',
+    async () => {
+        try {
+            const querySnapshot = await getDocs(collection(db, "appoinment"));
+            let data = [];
+            querySnapshot.forEach((doc) => {
+                data.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+                console.log(data);
 
-export const appoinmentfetch = createAsyncThunk(
+            });
+            return data
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+
+
+    }
+
+)
+export const addappoinment = createAsyncThunk(
     'appointment/fetch',
     async (data) => {
         console.log(data, "appoiment slice");
@@ -25,25 +47,34 @@ export const appoinmentfetch = createAsyncThunk(
         }
     }
 )
-export const getappoinment = createAsyncThunk(
-    'appointment/fetch',
+
+export const deleteappoinment = createAsyncThunk(
+    'appointment/delete',
     async (id) => {
+        console.log(id, "appoiment slice id");
         try {
-            const querySnapshot = await getDocs(collection(db, "appoinment"),id);
-            querySnapshot.forEach((doc) => {
-                console.log(`${doc.id} `,"listttt");
-                return {
-                    id:doc.id,
-                    
-                }
-            });
-          
+            await deleteDoc(doc(db, "appoinment", id));
+            return id
         } catch (e) {
             console.error("Error adding document: ", e);
         }
     }
-
 )
+export const updateappoinment = createAsyncThunk(
+    'appointment/update',
+    async (data) => {
+        console.log(data, "appoiment slice up-data");
+        try {
+            const aptRef = doc(db, "appoinment", data.id);
+            await updateDoc(aptRef, data);
+            return data
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    }
+)
+
+
 const onRejected = (state, action) => {
     state.isloading = false;
     state.error = action.error.message
@@ -60,10 +91,48 @@ export const appoinmentslice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
-            .addCase(appoinmentfetch.pending, onLoading)
-            .addCase(appoinmentfetch.fulfilled, (state, action) => {
-                console.log(action.payload,"action");
+            .addCase(getappoinment.rejected, onRejected)
+            .addCase(getappoinment.pending, onLoading)
+            .addCase(getappoinment.fulfilled, (state, action) => {
+                // console.log(action.payload, "action");
                 state.apt = action.payload
+                state.isloading = false;
+                state.error = null;
+
+            })
+            .addCase(addappoinment.rejected, onRejected)
+            .addCase(addappoinment.pending, onLoading)
+            .addCase(addappoinment.fulfilled, (state, action) => {
+                // console.log(action.payload,"action");
+                state.apt = state.apt.concat(action.payload)
+                state.isloading = false;
+                state.error = null;
+            })
+
+            .addCase(deleteappoinment.rejected, onRejected)
+            .addCase(deleteappoinment.pending, onLoading)
+            .addCase(deleteappoinment.fulfilled, (state, action) => {
+                console.log(action.payload, "action delete");
+                state.apt = state.apt.filter((v) => v.id !== action.payload)
+                state.isloading = false;
+                state.error = null;
+            })
+
+            .addCase(updateappoinment.rejected, onRejected)
+            .addCase(updateappoinment.pending, onLoading)
+            .addCase(updateappoinment.fulfilled, (state, action) => {
+                console.log(action.payload, "action update");
+                let udata = state.apt.map((v) => {
+                    if (v.id === action.payload.id) {
+                        return action.payload
+                    } else {
+                        return v
+                    }
+                })
+
+                state.apt = udata
+                state.isloading = false;
+                state.error = null;
             })
     }
 
